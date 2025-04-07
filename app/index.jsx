44dd33f3c229834c322d2a42084c4fdc6061fec9
prabath1998 <prabhathudayanga1998@gmail.com>
2025-04-07 18:09:54 +0530

    implement auth screens

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Image } from "expo-image";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Books</Text>
-      <Image
-        source={{
-          uri: "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U",
-        }}
-        style={{ width: 200, height: 300 }}
-      />
+     <Link href={"/(auth)/signup"}>Signup Page</Link>
+     <Link href={"/(auth)"}>Login Page</Link>
     </View>
   );
 }
